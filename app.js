84c36124dc9b3e556ff8fce9450fb28035047a90
Anonna Ferdaus <anonnaferdosh193@gmail.com>
2023-02-02 23:45:33 +0600

    js practice

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log(7);
+/* var numbers= [11, 22, 33, 44, 55, 66, 77];
+
+for(var i=0; i<numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+
+//break.js
+
+/* for(var i=1; i<=20; i++){
+    console.log(i);
+    if(i > 10){
+        break;
+    }
+}
+ */
+
+//function
+/* 
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+//function call
+startFan();
+
+startFan();
+startFan(); */
+
+
+function bringSingara(money){
+    console.log('mama singara den');
+    console.log(money);
+}
+
+bringSingara(250);
